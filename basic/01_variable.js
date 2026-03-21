79b91bdea68617a231a97eb0521e52d5539be859
Deepak Yadav <119037072+Deepak2200@users.noreply.github.com>
2026-03-21 05:09:08 +0000

    variable

---
+++ basic/01_variable.js
@@ -0,0 +1,20 @@
+const accountid = 00001
+let account_mail ="Deepak@yopmail.com"
+var company_name = "kotak mahindra bank"
+
+account_city = "Mumbai"
+
+// accountid = 2  // not allowed because accountid is a constant variable
+account_mail = "deepak123@yopmail.com"
+account_city = "Delhi"
+company_name = "Sbi"   
+
+
+
+console.table([accountid,account_mail,account_city,company_name])
+
+/*
+1. const - constant variable, cannot be reassigned, must be initialized at the time of declaration
+2. let - block scope variable, can be reassigned, can be declared without initialization
+3. var - function scope variable, can be reassigned, can be declared without initialization
+4. implicit global variable - if a variable is assigned a value without declaring it with var, let or const, it becomes a global variable
