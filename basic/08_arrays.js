778edcfcdc60b6e6a3e9ccc80457e1d7dc99b96d
Deepak Yadav <119037072+Deepak2200@users.noreply.github.com>
2026-03-26 13:59:54 +0000

    array

---
+++ basic/08_arrays.js
@@ -0,0 +1,53 @@
+// Arrays
+
+// const arr = [1, 2, 3, 4, 5]
+// console.log(arr)
+// console.log(typeof arr) // object
+// console.log(Array.isArray(arr)) // true
+
+// console.log(arr[0])
+
+
+// const arry1 = new Array(1,2,3,4,6)
+// console.log(arry1)
+// console.log(arry1.entries(10))
+
+
+// Arrays methods 
+
+
+// arr.push(6) // add element at the end of the array
+// // console.log(arr)
+// arr.pop() // remove the last element of the array
+// // console.log(arr)
+
+// arr.unshift(0) // add element at the beginning of the array
+// // console.log(arr)
+// arr.shift() // remove the first element of the array
+
+// arr.shift()
+
+// arr = [1, 2, 3, 4, 5]
+// console.log(arr.indexOf(3))
+// console.log(arr.indexOf(9)) // 2
+
+
+// const arr  = [1,2,3,5,6,1]
+
+// // console.log(arr , typeof arr)
+
+// // const new_arr = arr.join()
+// // console.log(new_arr, typeof new_arr)
+
+// // Slice and splice
+
+// console.log("A " ,arr)
+
+// const sliced_arr = arr.slice(1,4) // slice(start, end) end is not included
+// console.log(sliced_arr)
+
+
+// console.log("B " ,arr)
+// const spliced_arr = arr.splice(1,4) // splice(start, deleteCount)
+// console.log("c" ,arr)
+
